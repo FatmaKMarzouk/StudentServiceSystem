28d07ff55c00c4f95c8f4a6da35b72a0967167aa
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-03-07 22:21:58 +0200

    1

---
+++ src/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express ();
+var mysql = require('mysql');
+var port = process.env.PORT || 3000;
+
+app.use('/assets',express.static(__dirname + '/public'));
+app.set('view engine','ejs');
+app.use('/',function(req,res,next){
+  console.log('Request URL :'+ req.url);
+  res.send('heyy');
+  var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'addressbook',
+  });
+  con.connect();
+
+  next();
+});
+
+app.listen(port); //this function does http.createServer
