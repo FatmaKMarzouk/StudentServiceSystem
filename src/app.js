22b1ea8f3843184c39fae7c4bbca3f2bac97c096
Diaa Saber <diaa_msaber@hotmail.com>
2020-03-10 01:57:03 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use('/',function(req,res,next){
       console.log(err.code);
       console.log(err.fatal);
       console.log(err.sqlState);
+      console.log(err.sqlMessage);
       return;
     }
 
