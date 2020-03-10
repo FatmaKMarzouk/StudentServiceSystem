12203bffae8bfd4a4db4c3046e76d23e97050046
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-03-10 19:45:51 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,13 +15,12 @@ app.use('/',function(req,res,next){
   console.log('Request URL :'+ req.url);
   res.send('heyy');
   var connection = mysql.createConnection({
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT,
-    dbname   : process.env.RDS_DB_NAME
+    host     : RDS_HOSTNAME,
+    user     : RDS_USERNAME,
+    password : RDS_PASSWORD,
+    port     : RDS_PORT,
+    dbname   : RDS_DB_NAME
   });
-
   connection.connect(function(err) {
     if (err) {
       console.error('Database connection failed: ' + err.stack);
