2dbab587bab25044288ebea901938f82e4a640c9
alyzain <52960411+alyzain@users.noreply.github.com>
2020-03-11 15:24:36 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,45 +1,44 @@
-var express = require('express');
+var express = require("express");
 
-var app = express ();
-var mysql = require('mysql');
+var app = express();
+var mysql = require("mysql");
 var port = process.env.PORT || 3000;
-var RDS_HOSTNAME ='alexuni.csmzzxbw7ojv.us-east-2.rds.amazonaws.com';
-var RDS_PORT = '3306';
-var RDS_DB_NAME= 'AlexUni';
-var RDS_USERNAME = 'Unified7';
-var RDS_PASSWORD = 'Unified7!!';
-app.use('/assets',express.static(__dirname + '/public'));
-app.set('view engine','ejs');
-console.log('hi');
-console.log('hana');
-app.use('/',function(req,res,next){
-  console.log('Request URL :'+ req.url);
-  res.send('heyy');
+var RDS_HOSTNAME = "alexuni.csmzzxbw7ojv.us-east-2.rds.amazonaws.com";
+var RDS_PORT = "3306";
+var RDS_DB_NAME = "AlexUni";
+var RDS_USERNAME = "Unified7";
+var RDS_PASSWORD = "Unified7!!";
+app.use("/assets", express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+console.log("hi");
+app.use("/", function(req, res, next) {
+  console.log("Request URL :" + req.url);
+  res.send("heyy");
   var connection = mysql.createConnection({
-    host     : RDS_HOSTNAME,
-    user     : RDS_USERNAME,
-    password : RDS_PASSWORD,
-    port     : RDS_PORT,
-    dbname   : RDS_DB_NAME
+    host: RDS_HOSTNAME,
+    user: RDS_USERNAME,
+    password: RDS_PASSWORD,
+    port: RDS_PORT,
+    dbname: RDS_DB_NAME
   });
   connection.connect(function(err) {
     if (err) {
       console.log({
-        host            : RDS_HOSTNAME,
-        user            : RDS_USERNAME,
-        password        : RDS_PASSWORD,
-        database        : RDS_DB_NAME
-     })
-      console.error('Database connection failed: ' + err.stack);
+        host: RDS_HOSTNAME,
+        user: RDS_USERNAME,
+        password: RDS_PASSWORD,
+        database: RDS_DB_NAME
+      });
+      console.error("Database connection failed: " + err.stack);
       return;
     }
 
-    console.log('Connected to database.');
+    console.log("Connected to database.");
 
-    console.log('Connected to database hana.');
+    console.log("Connected to database hana.");
   });
 
- connection.end();
+  connection.end();
   next();
 });
 
