bee24c8456b5233e6c1344a651f5b65606e7aa78
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-03-17 01:19:40 +0200

    1

--- src/routes/chooseprog.js
+++ src/routes/chooseprog.js
@@ -13,24 +13,30 @@ var connection = require('../controllers/dbconnection');
 
 router.get('/chooseprog', function(request, response,next) {
   if (request.session.loggedin) {
+    response.send('html');
     var username = request.session.username;
+    var facultyname=""; var ssp="";
     connection.query('USE AlexUni');
-    connection.query('SELECT Faculty,SSP FROM Students WHERE Username = ? ', [username], function(error, results, fields) {
-			if (results.length>0) {
-      Object.keys(result).forEach(function(key) {
-      var row = result[key];
-      console.log(row.Faculty);
+    connection.query('SELECT Faculty,SSP FROM Students WHERE Username = ? ', [username], function(error, results1, fields) {
+			if (results1.length>0) {
+      Object.keys(results1).forEach(function(key) {
+      var row = results1[key];
+      facultyname = row.Faculty;
+      ssp = row.SSP;
+      });
+    connection.query('USE AlexUni');
+    connection.query('SELECT Name FROM Program WHERE FacultyName = ? AND SSP = ?',[facultyname,ssp],function(error,results2,fields){
+      if (results2.length>0){
+        console.log(results2);
+      }
+      else {
+        console.log("no valid program available");
+      }
     });
-
-
-			} else {
-				response.send('Incorrect Data!No Faculty name specified');
-			}
-
-
-
-		});
-      response.sendFile(__dirname+'/home.html');
+  }else {
+      console.log('Incorrect Data!No Faculty name specified');
+    }
+    });
   } else {
     response.send('Please login to view this page!');
   }
