43d7f89dd9c96fb435b68781902dbcbaf16653e9
Hana Ayman <hanaayman1997@gmail.com>
2020-03-16 22:10:33 +0200

    hana

--- src/app.js
+++ src/app.js
@@ -8,9 +8,11 @@ var home = require("./routes/home")
 var login = require("./routes/login");
 var postpone = require("./routes/postpone");
 var homesec = require("./routes/homesec");
+var enrollement = require("./routes/enrollement");
 app.use(login);
 app.use(home);
 app.use(postpone);
 app.use(homesec);
+app.use(enrollement);
 app.listen(port); //this function does http.createServer
 module.exports = app;
