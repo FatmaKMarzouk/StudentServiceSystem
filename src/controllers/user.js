234c47837d45e6fb91dcc7480bbf277dbeb110c6
Diaa Saber <diaa_msaber@hotmail.com>
2020-03-17 19:20:57 +0200

    1

---
+++ src/controllers/user.js
@@ -0,0 +1,23 @@
+var connection = require('../controllers/dbconnection');
+
+function User() {};
+
+User.prototype = {
+    // Find the user data byusername.
+    find : function(user = null, callback)
+    {
+        // prepare the sql query
+        let sql = `SELECT * FROM users WHERE Username = ?`;
+
+
+        connection.query(sql, user, function(err, result) {
+            if(err) throw err
+
+            if(result.length) {
+                callback(result[0]);
+            }else {
+                callback(null);
+            }
+        });
+    },
+}
