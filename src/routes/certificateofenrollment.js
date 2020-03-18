a99510357f7b05f67d0960f879b4c3027125fb75
Diaa Saber <diaa_msaber@hotmail.com>
2020-03-18 16:34:38 +0200

    Update certificateofenrollment.js

--- src/routes/certificateofenrollment.js
+++ src/routes/certificateofenrollment.js
@@ -13,36 +13,32 @@ router.get('/certificateofenrollment', function(req,res,next){
     var resultobject1='';
     var resultobject2='';
     connection.query('Use AlexUni');
-    connection.query('SELECT * FROM Students WHERE Username = ?', [username], function(err,results,field){
+    connection.query('SELECT NameEN,NameAr,Faculty,Program FROM Students WHERE Username = ?', [username], function(err,results,field){
         if(results.length>0)
         {
             Object.keys(results).forEach(function(key) {
                 var row = results[key];
-                    fullnameEN = row.NameEn,
-                    fullnameAR = row.NameAr,
-                    faculty = row.Faculty,
-                    program = row.Program
+                console.log(row);
+                resultobject1=row;
                 });
-                console.log(results);
+                return;
              /*   console.log("1st result " + results);
                 res.send(`${results}\n2nd results\n`);*/
         }
     });
     connection.query('Use IntegratedData');
-    connection.query('SELECT GPA FROM Student WHERE ID = ?', [username], function(err,results,field){
+    connection.query('SELECT GPA,Semester FROM Student WHERE ID = ?', [username], function(err,results,field){
         if(results.length>0)
         {
               Object.keys(results).forEach(function(key) {
                 var row = results[key];
-                resultobject2 = {
-                    term: row.Semester,
-                    gpa: row.GPA
-                };
-                console.log(term,gpa);
+                resultobject2=row;
+                console.log(row);
                 return;
                 });
+                res.send('result 1: ' + resultobject1 + '\n\n\n\n result 2: ' + resultobject2);
         }
     //    res.send('\n2nd Result\n' + results);
     });
-        //res.send('result 1: ' + resultobject1 + '\n\n\n\n result 2: ' + resultobject2);
+        
 });
