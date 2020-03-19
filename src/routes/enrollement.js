2a2461f534529ce749a4720fc872685ccdcbfd0a
Hana Ayman <hanaayman1997@gmail.com>
2020-03-19 18:03:20 +0200

    s

--- src/routes/enrollement.js
+++ src/routes/enrollement.js
@@ -11,32 +11,50 @@ var nodemailer = require('nodemailer');
 router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 
+var facultyesc;
+
 router.get('/enrollement', function(request, response,next) {
 	response.sendFile(__dirname+'/enrollement.html');
 	//console.log('its anhoon11');
+
 	if (request.session.loggedin) {
 		var secusername = request.session.username;
 		
-		connection.query('SELECT FacultyName FROM Secretary WHERE username = ? ',[secusername] ,  function(error, results, fields){
+		connection.query('SELECT FacultyName FROM Secretary WHERE ID = ? ',[secusername] ,  function(error, results, fields){
 
-			//if (results.length>0){
-				console.log(results);  
-				var faculty = results;
-				console.log(faculty); 
-			//  }
-			//   else {
-			// 	console.log("3aaaaaaaaaaaa");
+			// if (results.length>0){
+			// 	console.log(results);  
+			// 	facultyesc = JSON.stringify(results.FacultyName);
+			// 	console.log("fooooooooooooooo");
+			// 	console.log(facultyesc); 
 			//   }
+			if(results.length>0){
+				console.log("hhhhhooooooooooooo");
+				Object.keys(results).forEach(function(key){
+				  var row = results[key];
+				  facultyesc = row.FacultyName;
+				  console.log("fooooooooooooooo");
+			      console.log(facultyesc); 
+				});}
+			else {
+			 	console.log("3aaaaaaaaaaaa");
+			   }
+
 
 		});
 	}
-	console.log(secusername);
+
 });
 
+console.log("woooooo");
+console.log(facultyesc); 
 var connection = require('../controllers/dbconnection');
 
+
 router.post('/enroll', function(request, response) {
-	//console.log('its anhoon');
+
+	console.log("plzzzzzzzzz");
+	//console.log(facultysec);
 	response.send('Student has been added successfully');
 
 	var nameen = request.body.nameen;
@@ -53,11 +71,10 @@ router.post('/enroll', function(request, response) {
 	//var password = request.body.password;
 	var gender = request.body.gender;
 	var program = request.body.program;
-	var faculty = request.body.faculty;
+	//var faculty = request.body.faculty;
 	var birthCerftificate = request.body.birthCerftificate;
 	var nationalid = request.body.nationalid;
 	var nominationCard = request.body.nominationCard;
-
 	
 		connection.query('USE AlexUni');
 
@@ -70,11 +87,12 @@ router.post('/enroll', function(request, response) {
 
 
 		//connection.query('INSERT INTO Students (NameEn,NameAr,emergencyContact,Gender,medicalCondition,Email,Nationality,Birthdate,SSN,phoneNumber,Address,Username,Password,Faculty,Program) VALUES (nameen, namear, emergencycontact, gender, medicalcondition, email, nationality, birthdate, ssn, phonenumber, address, username, password,faculty,program) ',  function(error, results, fields){
-		connection.query('INSERT INTO Students (NameEn,NameAr,emergencyContact,Gender,medicalCondition,Email,Nationality,Birthdate,SSN,phoneNumber,Address,Username,Password,Faculty,Program) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ',[nameen, namear, emergencycontact, gender, medicalcondition, email, nationality, birthdate, ssn, phonenumber, address, username, result ,faculty,program] ,  function(error, results, fields){
+		// connection.query('INSERT INTO Students (NameEn,NameAr,emergencyContact,Gender,medicalCondition,Email,Nationality,Birthdate,SSN,phoneNumber,Address,Username,Password,Faculty,Program) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ',[nameen, namear, emergencycontact, gender, medicalcondition, email, nationality, birthdate, ssn, phonenumber, address, username, result ,facultysec, program] ,  function(error, results, fields){
 
-			if (error) throw error;
+		// 	console.log("ana heenaaaaaaaaaaa")
+		// 	if (error) throw error;
 
-		});
+		// });
 
 		const output = `
 			<p>You have been accepted in Alexandria University</p>
@@ -101,7 +119,7 @@ router.post('/enroll', function(request, response) {
 		  });
 
 		let mailOptions = {
-			from: '"Nodemailer Contact" <hanaayman1997@gmail.com>', // sender address
+			from: '"Alexandria University" <hanaayman1997@gmail.com>', // sender address
 			to: email, // list of receivers
 			subject: 'Alexandria University', // Subject line
 			text: 'Hello world?', // plain text body
