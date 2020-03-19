a3b0d8c6810c2a334e6fa33f653dc7759e66f152
Hana Ayman <hanaayman1997@gmail.com>
2020-03-19 21:10:56 +0200

    haha

--- src/routes/enrollement.js
+++ src/routes/enrollement.js
@@ -60,7 +60,6 @@ router.post('/enroll', function(request, response) {
 	var birthdate = request.body.birthdate;
 	var phonenumber = request.body.phonenumber;
 	var address = request.body.address;
-	var username = request.body.username;
 	var gender = request.body.gender;
 	var private = request.body.private;
 	var birthCerftificate = request.body.birthCerftificate;
@@ -101,56 +100,56 @@ router.post('/enroll', function(request, response) {
 					  console.log(user); 
 					  return user;
 					});}
-				else {
-					 console.log("3aaaaaaaaaaaa");
-				   }
+					const output = `
+					<p>You have been accepted in Alexandria University</p>
+					<h3>Contact Details</h3>
+					<ul>  
+					<li>Username: ${user}</li>
+					<li>Password: ${result}</li>
+					</ul>
+					<h3>Message</h3>
+					<p>Please login and change your password</p>
+				`;
+		
+				let transporter = nodemailer.createTransport({
+					host: 'smtp.gmail.com',
+					port: 587,
+					secure: false, // true for 465, false for other ports
+					auth: {
+						user: 'alexandriauniversity7@gmail.com', // generated ethereal user
+						pass: 'Unified7!!'  // generated ethereal password
+					},
+					tls:{
+					  rejectUnauthorized:false
+					}
+				  });
+		
+				let mailOptions = {
+					from: '"Alexandria University" <hanaayman1997@gmail.com>', // sender address
+					to: email, // list of receivers
+					subject: 'Alexandria University', // Subject line
+					text: 'Hello world?', // plain text body
+					html: output // html body
+				};
+		
+				transporter.sendMail(mailOptions, (error, info) => {
+					if (error) {
+						return console.log(error);
+					}
+					console.log('Message sent: %s', info.messageId);   
+					console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+			  
+					res.render('contact', {msg:'Email has been sent'});
+				});
 				
 			});
 
 			console.log("ana heenaaaaaaaaaaa")
 			if (error) throw error;
 
+
+
 		});
 
-		const output = `
-			<p>You have been accepted in Alexandria University</p>
-			<h3>Contact Details</h3>
-			<ul>  
-			<li>Username: ${user}</li>
-			<li>Password: ${result}</li>
-			</ul>
-			<h3>Message</h3>
-			<p>Please login and change your password</p>
-		`;
 
-		let transporter = nodemailer.createTransport({
-			host: 'smtp.gmail.com',
-			port: 587,
-			secure: false, // true for 465, false for other ports
-			auth: {
-				user: 'alexandriauniversity7@gmail.com', // generated ethereal user
-				pass: 'Unified7!!'  // generated ethereal password
-			},
-			tls:{
-			  rejectUnauthorized:false
-			}
-		  });
-
-		let mailOptions = {
-			from: '"Alexandria University" <hanaayman1997@gmail.com>', // sender address
-			to: email, // list of receivers
-			subject: 'Alexandria University', // Subject line
-			text: 'Hello world?', // plain text body
-			html: output // html body
-		};
-
-		transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				return console.log(error);
-			}
-			console.log('Message sent: %s', info.messageId);   
-			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-	  
-			res.render('contact', {msg:'Email has been sent'});
-		});
 });
