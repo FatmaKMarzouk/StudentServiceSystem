6987b7fa096867e8de83d0b7be65532277f895b9
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-03-22 14:54:15 +0200

    1

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -9,3 +9,10 @@ var path = require('path');
 router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 var connection = require('../controllers/dbconnection');
+
+router.get('/checkout',function(request,response,next){
+   //bab3at page el checkout.html hena ely fyha el fields ey tetmala bl credit card number w hakaza
+});
+router.post('/payment',function(request,response,next){
+  // hena ba2ra mel request.body el ma3lomat that he submitted 3an el card 3shan adyha lel bank b2a aw whatever.
+});
