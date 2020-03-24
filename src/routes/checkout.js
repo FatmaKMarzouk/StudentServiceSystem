f4419c55a6e4f0838bdabe3e8d8bc3bd995552c6
Diaa Saber <diaa_msaber@hotmail.com>
2020-03-24 21:56:19 +0200

    1

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,29 +1,125 @@
-var mysql = require('mysql');
-var express = require('express');
+// var mysql = require('mysql');
+// var express = require('express');
+// var router = express.Router();
+// module.exports = router;
+// var session = require('express-session');
+// var bodyParser = require('body-parser');
+// var path = require('path');
+// var fs = require('fs');
+
+// router.use(bodyParser.urlencoded({extended : true}));
+// router.use(bodyParser.json());
+// var connection = require('../controllers/dbconnection');
+
+// router.get('/checkout',function(req,res,next){
+//    //bab3at page el checkout.html hena ely fyha el fields ey tetmala bl credit card number w hakaza
+
+//    fs.readFile('items.json', function(error, data) {
+//      if(error) {
+//       res.status(500).end();
+//      } else {
+//        res.render('checkout.ejs', {
+//          stripePublicKey: stripePublicKey,
+//          items: JSON.parse(data)
+//        })
+//      }
+//    });
+// });
+// router.post('/payment',function(request,response,next){
+//   // hena ba2ra mel request.body el ma3lomat that he submitted 3an el card 3shan adyha lel bank b2a aw whatever.
+// });
+
+
+const express = require("express");
 var router = express.Router();
 module.exports = router;
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
-var fs = require('fs');
 
-router.use(bodyParser.urlencoded({extended : true}));
-router.use(bodyParser.json());
-var connection = require('../controllers/dbconnection');
 
-router.get('/checkout',function(req,res,next){
-   //bab3at page el checkout.html hena ely fyha el fields ey tetmala bl credit card number w hakaza
+const { resolve } = require("path");
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-   fs.readFile('items.json', function(error, data) {
-     if(error) {
-      res.status(500).end();
-     } else {
-       res.render('checkout.html', {
-         items: JSON.parse(data)
-       })
-     }
-   });
+router.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function(req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    }
+  })
+);
+
+router.get("/checkout", (req, res) => {
+  // Display checkout page
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(__dirname+'../views/checkout.html');
 });
-router.post('/payment',function(request,response,next){
-  // hena ba2ra mel request.body el ma3lomat that he submitted 3an el card 3shan adyha lel bank b2a aw whatever.
+
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+router.post("/create-payment-intent", async (req, res) => {
+  const { items, currency } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: currency
+  });
+
+  // Send publishable key and PaymentIntent details to client
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    clientSecret: paymentIntent.client_secret
+  });
+});
+
+// Expose a endpoint as a webhook handler for asynchronous events.
+// Configure your webhook in the stripe developer dashboard
+// https://dashboard.stripe.com/test/webhooks
+router.post("/webhook", async (req, res) => {
+  let data, eventType;
+
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers["stripe-signature"];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // we can retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === "payment_intent.succeeded") {
+    // Funds have been captured
+    // Fulfill any orders, e-mail receipts, etc
+    // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+    console.log("💰 Payment captured!");
+  } else if (eventType === "payment_intent.payment_failed") {
+    console.log("❌ Payment failed.");
+  }
+  res.sendStatus(200);
 });
