e453ca9c11094e6fe42fe3195972d5f240c936bd
rasheedy97 <46133878+rasheedy97@users.noreply.github.com>
2020-04-13 23:27:34 +0200

    new update

--- src/routes/transcript.js
+++ src/routes/transcript.js
@@ -5,13 +5,16 @@ module.exports = router;
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
-var  courses=[]
+var courses = []
 const officegen = require('officegen')
 const fs = require('fs')
-var masterobject="";
-
-
-
+var masterobject = "";
+var name = "";
+var id = ""
+var flag = 1;
+var totalReg = "";
+var totalEarned = "";
+var prog="";
 
 
 router.use(bodyParser.urlencoded({
@@ -24,73 +27,74 @@ router.get('/transcript', function (request, response, next) {
     if (request.session.loggedin) {
         response.sendFile(__dirname + '/transcript.html');
         var username = request.session.username;
-        var id = "";
-        var name = "";
-        var flag = 1;
-        var totalReg= "";
-        var totalEarned= "";
+
         connection.query('USE IntegratedData');
         connection.query('SELECT ID,Name,ProgramName,TotalRegHours,TotalEarnedHours FROM Student WHERE ID = ? ', [username], function (error, results, fields) {
             var info = [];
             if (results.length > 0) {
 
                 Object.keys(results).forEach(function (key) {
-                   row1=results[key]
+                    row1 = results[key]
                     // info.push(results[key]);
-                   id = row1.ID;
+                    id = row1.ID;
                     prog = row1.ProgramName;
                     name = row1.Name;
-                totalReg =row1.TotalRegHours
-                totalEarned = row1.TotalEarnedHours
-               // console.log(info)
-            });
+                    totalReg = row1.TotalRegHours
+                    totalEarned = row1.TotalEarnedHours
+                    // console.log(info)
+                });
 
-                console.log(id, name, prog,totalReg,totalEarned);
-               connection.query('USE IntegratedData');
+                console.log(id, name, prog, totalReg, totalEarned);
+                connection.query('USE IntegratedData');
                 connection.query('SELECT CourseName,Grade,Semester FROM EnrolledCourses WHERE StudentID = ?  ORDER BY SemesterNum ASC', [id], function (error, results2, fields) {
 
                     if (results2.length > 0) {
 
                         Object.keys(results2).forEach(function (key) {
                             //courses.push(results2[key]);
-                            masterobject = {...masterobject,...results2};
+                            masterobject = {
+                                ...masterobject,
+                                ...results2
+                            };
                         });
-                              console.log('bada2t teba3a\n');
-                              console.log(masterobject);
-                              console.log('\n5allast teba3a\n');
-                              connection.query('USE IntegratedData');
-                              connection.query('SELECT Semester,GPA,regHours FROM Semesters WHERE StudentID = ?',[id],function (error, results4, fields) {
-                                    var termGpa = [];
-                                  if (results4.length > 0) {
+                        console.log('bada2t teba3a\n');
+                        console.log(masterobject);
+                        console.log('\n5allast teba3a\n');
+                        connection.query('USE IntegratedData');
+                        connection.query('SELECT Semester,GPA,regHours FROM Semesters WHERE StudentID = ?', [id], function (error, results4, fields) {
+                            var termGpa = [];
+                            if (results4.length > 0) {
 
-                                      Object.keys(results4).forEach(function (key) {
-                                        //  termGpa.push(results4[key]);
-                                         masterobject = {...masterobject,...results4};
+                                Object.keys(results4).forEach(function (key) {
+                                    //  termGpa.push(results4[key]);
+                                    masterobject = {
+                                        ...masterobject,
+                                        ...results4
+                                    };
 
-                                      });
-                                      console.log('bada2t teba3a tanyyy\n');
-                                      console.log(masterobject);
-                                      console.log('\n5allast teba3a tanyyy\n');
-                                  }
-                                  else{
-                                  response.send('Wrong ID');
-                                  }
-
-                              });
-
-
-
-
-
+                                });
+                                console.log('bada2t teba3a tanyyy\n');
+                                console.log(masterobject);
+                                console.log('\n5allast teba3a tanyyy\n');
                             } else {
+                                response.send('Wrong ID');
+                            }
+
+                        });
+
+
+
+
+
+                    } else {
                         response.send("no registered courses");
-                        flag =0;
+                        flag = 0;
                     }
                 });
 
             } else {
                 response.send("Not a registered student");
-                flag=0;
+                flag = 0;
             }
 
 
@@ -108,94 +112,111 @@ router.get('/transcript', function (request, response, next) {
 
 
 });
-router.get('/transcriptconfirm', function(request, response,fields) {
-    if (request.session.loggedin){
+router.get('/transcriptconfirm', function (request, response, fields) {
+    if (request.session.loggedin) {
 
         var username = request.session.username;
         var paid = "";
-         var flag=1;
+        var flag = 1;
         connection.query('USE AlexUni');
         connection.query('SELECT Paid FROM Payment WHERE StudentID = ? ', [username], function (error, results3, fields) {
-            if (results3.length > 0){
+            if (results3.length > 0) {
                 Object.keys(results3).forEach(function (key) {
                     var row3 = results3[key];
-                    paid =row3.Paid ;
-                     });
-                      if (paid){                      
-                            // Create an empty Word object:
-let docx = officegen('docx')
- 
-// Officegen calling this function after finishing to generate the docx document:
-docx.on('finalize', function(written) {
-  console.log(
-    'Finish to create a Microsoft Word document.'
-  )
-})
- 
-// Officegen calling this function to report errors:
-docx.on('error', function(err) {
-  console.log(err)
-})
- 
-// Create a new paragraph:
-let pObj = docx.createP()
- 
-pObj = docx.createP({align: 'center'})
+                    paid = row3.Paid;
+                });
+                if (paid) {
+                    // Create an empty Word object:
+                    let docx = officegen('docx')
 
-// We can even add images:
-pObj.addImage(path.resolve(__dirname,'uni_logoo.png'), {cx: 120, cy: 120})
+                    // Officegen calling this function after finishing to generate the docx document:
+                    docx.on('finalize', function (written) {
+                        console.log(
+                            'Finish to create a Microsoft Word document.'
+                        )
+                    })
+
+                    // Officegen calling this function to report errors:
+                    docx.on('error', function (err) {
+                        console.log(err)
+                    })
+
+                    // Create a new paragraph:
 
 
-pObj = docx.createP({ align: 'left' })
-pObj.addText("Studnet's name :")
-pObj.addLineBreak()
-pObj.addText("Studnet's ID :")
-//pObj = docx.createP()
- //pObj.addText('Bold + underline', { bold: true, underline: true })
- //pObj = docx.createP()
- //pObj.addText('Those two lines are in the same paragraph,')
-//pObj.addLineBreak()
-//pObj.addText('but they are separated by a line break.')
-docx.putPageBreak()
- //pObj = docx.createP()
-//pObj.addText('Fonts face only.', { font_face: 'Arial' })
-//pObj.addText(' Fonts face and size.', { font_face: 'Arial', font_size: 40 })
- //docx.putPageBreak()
+                    pObj = docx.createP({
+                        align: 'center'
+                    })
 
-// Let's generate the Word document into a file:
- let out = fs.createWriteStream('example.docx')
- out.on('error', function(err) {
-  console.log(err)
-})
- // Async call to generate the output file:
-docx.generate(out)
-                                 
-                          if (flag==1){
+                    // We can even add images:
+                    pObj.addImage(path.resolve(__dirname, 'uni_logoo.png'), {
+                        cx: 120,
+                        cy: 120
+                    })
 
-                          connection.query('USE AlexUni');
-                          connection.query('INSERT INTO Requests (StudentID,ServiceName,Amount) VALUES( ?,?,? ) ',[username,"Request Transcript","50"]);
-                          response.redirect('/cart');
-                        }
-                      }
-                    else {
-                        response.send("You haven't paid fees");
-                        flag = 0;
+
+                    pObj = docx.createP({
+                        align: 'left'
+                    })
+                    pObj.addText("Studnet's name :"+name)
+                    //pObj.addText(name,{color : '0000A0', bold: true, underline: true})
+                    pObj.addLineBreak()
+                   pObj.addText("Student's ID : "+id)
+                   
+                    pObj.addLineBreak()
+                    pObj.addText("Student's program: "+prog)
+                    
+                    pObj.addLineBreak()
+                    pObj.addText("Student's total registered hours : "+totalReg)
+                    
+                    pObj.addLineBreak()
+                    pObj.addText("Student's total earned hours : "+totalEarned)
+                    pObj.addLineBreak()
+                    pObj.addText("Subject's objects :")
+                    
+                    
+                    //pObj = docx.createP()
+                    //pObj.addText('Bold + underline', { bold: true, underline: true })
+                    //pObj = docx.createP()
+                    //pObj.addText('Those two lines are in the same paragraph,')
+                    //pObj.addLineBreak()
+                    //pObj.addText('but they are separated by a line break.')
+                   // docx.putPageBreak()
+                    //pObj = docx.createP()
+                    //pObj.addText('Fonts face only.', { font_face: 'Arial' })
+                    //pObj.addText(' Fonts face and size.', { font_face: 'Arial', font_size: 40 })
+                    //docx.putPageBreak()
+
+                    // Let's generate the Word document into a file:
+                    let out = fs.createWriteStream('example.docx')
+                    out.on('error', function (err) {
+                        console.log(err)
+                    })
+                    // Async call to generate the output file:
+                    docx.generate(out)
+
+                    if (flag == 1) {
+
+                        connection.query('USE AlexUni');
+                        connection.query('INSERT INTO Requests (StudentID,ServiceName,Amount) VALUES( ?,?,? ) ', [username, "Request Transcript", "50"]);
+                        response.redirect('/cart');
                     }
-
-                    }
-                    else {
-
-
-                    }
-
-
-                    });
-
+                } else {
+                    response.send("You haven't paid fees");
+                    flag = 0;
                 }
 
-  else {
-      response.send('Please log in')
-  }
+            } else {
+
+
+            }
+
+
+        });
+
+    } else {
+        response.send('Please log in')
+    }
 
 
 
