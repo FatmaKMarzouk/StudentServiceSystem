897ec7ce3aa2502badd83e5da278ef2a2deba8e8
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-08-06 02:29:59 +0200

    1

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -23,7 +23,7 @@ function App() {
     }
     console.log("abl el axios.verifytoken");
     axios
-      .get(`http://ec2-18-222-90-155.us-east-2.compute.amazonaws.com:5000/verifyToken?token=${token}`)
+      .get(`http://18.222.90.155:5000/verifyToken?token=${token}`)
       .then((response) => {
         setUserSession(response.data.token, response.data.user);
         setAuthLoading(false);
