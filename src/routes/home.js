831d689aac0ba9a9717ebbf599bfaf0ea7537601
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-08-07 00:55:45 +0200

    1

--- src/routes/home.js
+++ src/routes/home.js
@@ -11,11 +11,14 @@ router.use(bodyParser.json());
 var connection = require('../controllers/dbconnection');
 var env = require("dotenv").config({ path: __dirname + "../.env" });
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
+var name = " ";
+var username = " ";
 router.get('/home', function(req, res,next) {
   if(req.user)
   {
-    
+
+    connection.query('USE AlexUni');
+
   }
   else{
     response.status(400).send({
