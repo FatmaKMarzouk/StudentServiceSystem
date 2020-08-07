4844add84686e9e5df5a7a9523b6c49ca23f9a23
Diaa Saber <diaa_msaber@hotmail.com>
2020-08-07 15:55:22 +0200

    Update Login.js

--- src/client/src/components/login/Login.js
+++ src/client/src/components/login/Login.js
@@ -20,7 +20,7 @@ function Login(props) {
     setError(null);
     setLoading(true);
     axios
-      .post("http://3.134.107.83:5000/users/signin", {
+      .post("http://localhost:5000/users/signin", {
         username: username.value,
         password: password.value,
         role: role.value,
