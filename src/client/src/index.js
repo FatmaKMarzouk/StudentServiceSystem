f73d1fea52b793e354156d5a180f462c5d2765af
FatmaKMarzouk <61886548+FatmaKMarzouk@users.noreply.github.com>
2020-08-09 00:40:26 +0200

    Revert "1"
    This reverts commit 84326416441370f5e9169b82d273823167722484.

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-<a href="https://seal.beyondsecurity.com/vulnerability-scanner-verification/ec2-3-134-107-83.us-east-2.compute.amazonaws.com"><img src="https://seal.beyondsecurity.com/verification-images/ec2-3-134-107-83.us-east-2.compute.amazonaws.com/vulnerability-scanner-2.gif" alt="Website Security Test" border="0"></a>
+
